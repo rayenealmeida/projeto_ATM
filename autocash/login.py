87e6d30faf2b03cb5852b45f0e3e15b1050c5373
almeidarayene <almeidarayene@gmail.com>
2023-06-04 20:57:26 -0300

    mensagem login

--- autocash/login.py
+++ autocash/login.py
@@ -4,11 +4,15 @@ from tkinter import Label
 from PIL import ImageTk, Image
 from tinydb import TinyDB, where
 
+
 # Obtém o caminho do diretório atual
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 caminho_banco_dados = os.path.join(diretorio_atual, 'banco_de_dados.json')
 
 db = TinyDB(caminho_banco_dados)
+
+
+
 
 def verificar_login():
     cpf = entry_cpf_login.get()
@@ -17,9 +21,9 @@ def verificar_login():
     resultado = db.search((where('cpf') == cpf) & (where('senha') == senha))
     
     if resultado:
-        print('Login realizado com sucesso!')
+        label_resultado['text'] = 'Login realizado com sucesso!'        
     else:
-        print('CPF ou senha inválidos!')
+        label_resultado['text'] = 'CPF ou senha inválidos!'
 
 janela_login = tk.Tk()
 janela_login.geometry("600x600")
@@ -50,5 +54,9 @@ entry_senha.place(x=100, y=200)
 button_login = tk.Button(janela_login, text= 'Entrar', command= verificar_login)
 button_login.pack()
 button_login.place(x=100, y=230)
+
+label_resultado = Label(janela_login, text='')
+label_resultado.pack()
+label_resultado.place(x=100, y=260)
 
 janela_login.mainloop()
