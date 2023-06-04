23f7cd773ebb8579e34316f629438651e449d6ba
Carlos Eduardo <eduardokadu831@gmail.com>
2023-06-04 20:44:20 -0300

    Atualizando

--- autocash/library/module.py
+++ autocash/library/module.py
@@ -1,2 +1,27 @@
-import random
 from datetime import datetime
+
+# class Conta:
+#     def __init__(self):
+    
+# class Transacoes:
+
+# class Cliente:
+
+class Gerente:
+    def __init__(self):
+        self.accounts = {}
+
+    def criar_conta(self, numero_conta, valor_inicial):
+         # Recupera os dados do login faz uma 'análise' e aprova ou não a criação de uma conta enviando os dados para a classe conta
+         print(f"Conta {numero_conta} criada com sucesso!")
+
+    def excluir_conta(self, numero_conta):
+        # Exclui uma determinada conta
+        print(f"Conta {numero_conta} excluída com sucesso!")
+
+    def solicitar_credito(self):
+        # Se valor solicitado <= renda*5 aprovado senão reprovado
+        print("Aprovado")
+
+
+          
