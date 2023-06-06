d83de13547c431e204547c1b12d7656200d783fd
Carlos Eduardo <eduardokadu831@gmail.com>
2023-06-06 13:02:18 -0300

    Atualizando

--- autocash/tempCodeRunnerFile.py
+++ autocash/tempCodeRunnerFile.py
@@ -1,25 +1,6 @@
-import os
-import tkinter as tk
-from tkinter import Label
-from PIL import ImageTk, Image
-
-diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-
-janela = tk.Tk()
-janela.geometry("600x600")
-janela.title("Menu")
-janela.resizable(False, False)
-
-imagem = Image.open(diretorio_atual + "/images/atm_bg.png")
-imagem = imagem.resize((600, 600), Image.ANTIALIAS)
-imagem_tk = ImageTk.PhotoImage(imagem)
-
-label = Label(janela, image=imagem_tk)
-label.place(x=0, y=0, relwidth=1, relheight=1)
-
 opcoes_texto = "Menu:\n\n1 - Extrato\n2 - Saque\n3 - Depósito\n4 - Realizar pagamento\n5 - Solicitar crédito\n6 - Sair\n\nEntre com a sua opção: _ "
 
-label_opcoes = Label(janela, text=opcoes_texto, font=("Montserrat", 14), justify="left", bg="#50c7e2")
+label_opcoes = Label(janela, text=opcoes_texto, font=("Montserrat", 16), justify="left", bg="#50c7e2")
 label_opcoes.pack()
 label_opcoes.place(x=90, y=90)
 
@@ -55,12 +36,4 @@ button_login = tk.Button(janela, text= '8', width=2)
 button_login.pack()
 button_login.place(x=166, y=476)
 
-button_login = tk.Button(janela, text= '9', width=2)
-button_login.pack()
-button_login.place(x=219, y=476)
-
-button_login = tk.Button(janela, text= '0', width=2)
-button_login.pack()
-button_login.place(x=166, y=512)
-
-janela.mainloop()
+button_login = tk.Button(janela,
