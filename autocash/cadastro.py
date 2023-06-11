fa66b4e602f9c70aaa9a3a5c2701665856e209ee
Carlos Eduardo <eduardokadu831@gmail.com>
2023-06-10 23:47:39 -0300

    Alterações no cadastro.

--- autocash/cadastro.py
+++ autocash/cadastro.py
@@ -1,15 +1,13 @@
 import os
-import os
+from datetime import datetime
 from library.module import Gerente, Conta, Transacoes, Cliente, SolicitaCredito
 from tkinter import Tk, Label, Entry, Button, Frame, Toplevel
 from tinydb import TinyDB
 
-# Obtém o caminho do diretório atual
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 caminho_banco_dados = os.path.join(diretorio_atual, 'banco_de_dados.json')
 
 db = TinyDB(caminho_banco_dados)
-
 
 class CadastroCliente:
     def __init__(self):
@@ -24,34 +22,43 @@ class CadastroCliente:
         renda = float(entry_renda.get())
         senha = entry_senha.get()
 
-        # Verificar se todos os campos estão preenchidos
-        if nome and telefone and data_nascimento and cpf_ou_cnpj and endereco and renda and senha:
-            cliente = Cliente(cpf_ou_cnpj, nome, telefone, endereco, data_nascimento, renda)
+        data_atual = datetime.now().date()
+        data_nascimento_check = datetime.strptime(data_nascimento, "%d/%m/%Y").date()
+        idade = data_atual.year - data_nascimento_check.year
 
-            # Salvar os dados no banco de dados
-            db.insert({
-                'nível': 1,
-                'nome': nome,
-                'telefone': telefone,
-                'data_nascimento': data_nascimento,
-                'cpf': cpf_ou_cnpj,
-                'endereco': endereco,
-                'renda': renda,
-                'senha': senha,
-            })
-
-            aprovado = self.gerente.aprovar_conta(cliente)
-            if aprovado:
-                self.exibir_janela_confirmacao()
-            else:
-                self.exibir_janela_reprovacao()
-
-            self.limpar_campos()
+        if (data_atual.month, data_atual.day) < (data_nascimento_check.month, data_nascimento_check.day):
+            idade -= 1
+        if idade < 18:
+            self.exibir_janela_reprovacao()
         else:
-            # Exibir uma mensagem de erro ou alerta informando que todos os campos devem ser preenchidos
-            # Aqui está um exemplo simples utilizando uma caixa de diálogo
-            import tkinter.messagebox as messagebox
-            messagebox.showerror("Erro", "Todos os campos devem ser preenchidos.")
+            # Verificar se todos os campos estão preenchidos
+            if nome and telefone and data_nascimento and cpf_ou_cnpj and endereco and renda and senha:
+                cliente = Cliente(cpf_ou_cnpj, nome, telefone, endereco, data_nascimento, renda)
+
+                # Salvar os dados no banco de dados
+                db.insert({
+                    'cadastro_nivel': 1,
+                    'nome': nome,
+                    'telefone': telefone,
+                    'data_nascimento': data_nascimento,
+                    'cpf': cpf_ou_cnpj,
+                    'endereco': endereco,
+                    'renda': renda,
+                    'senha': senha,
+                    'transacoes': '',
+                    'saldo': '',
+                })
+
+                aprovado = self.gerente.aprovar_conta(cliente)
+                if aprovado:
+                    self.exibir_janela_confirmacao()
+                else:
+                    self.exibir_janela_reprovacao()
+
+                self.limpar_campos()
+            else:
+                import tkinter.messagebox as messagebox
+                messagebox.showerror("Erro", "Todos os campos devem ser preenchidos.")
 
     def exibir_janela_confirmacao(self):
         janela_confirmacao = Toplevel(janela)
@@ -92,31 +99,27 @@ def abrir_janela_login():
 
 janela = Tk()
 janela.title("Solicitar Cadastro")
-largura = 400
-altura = 400
-x = (janela.winfo_screenwidth() - largura) // 2
-y = (janela.winfo_screenheight() - altura) // 2
-janela.geometry(f'{largura}x{altura}+{x}+{y}')
+janela.geometry('400x400')
 
 label_nome = Label(janela, text='Nome:')
 label_nome.pack()
 entry_nome = Entry(janela)
 entry_nome.pack()
 
-label_telefone = Label(janela, text='Telefone:')
-label_telefone.pack()
-entry_telefone = Entry(janela)
-entry_telefone.pack()
+label_cpf_ou_cnpj = Label(janela, text='CPF/CNPJ (somente números):')
+label_cpf_ou_cnpj.pack()
+entry_cpf_ou_cnpj = Entry(janela)
+entry_cpf_ou_cnpj.pack()
 
-label_data_nascimento = Label(janela, text='Data de Nascimento:')
+label_data_nascimento = Label(janela, text='Data de Nascimento (ex: 01/01/2001):')
 label_data_nascimento.pack()
 entry_data_nascimento = Entry(janela)
 entry_data_nascimento.pack()
 
-label_cpf_ou_cnpj = Label(janela, text='CPF/CNPJ:')
-label_cpf_ou_cnpj.pack()
-entry_cpf_ou_cnpj = Entry(janela)
-entry_cpf_ou_cnpj.pack()
+label_telefone = Label(janela, text='Telefone:')
+label_telefone.pack()
+entry_telefone = Entry(janela)
+entry_telefone.pack()
 
 label_endereco = Label(janela, text='Endereço:')
 label_endereco.pack()
@@ -128,7 +131,7 @@ label_renda.pack()
 entry_renda = Entry(janela)
 entry_renda.pack()
 
-label_senha = Label(janela, text='Crie uma Senha:')
+label_senha = Label(janela, text='Crie a sua senha:')
 label_senha.pack()
 entry_senha = Entry(janela, show='*')
 entry_senha.pack()
@@ -143,6 +146,5 @@ button_salvar.place(x=140, y=300)
 button_login = Button(janela, text='Login', command=abrir_janela_login)
 button_login.pack(side='top', padx=5)
 button_login.place(x=210, y=300)
-
 
 janela.mainloop()
--- autocash/main.py
+++ autocash/main.py
@@ -1,7 +1,8 @@
 import os, subprocess, tkinter as tk
+from tkinter import ttk
 from library.module import Gerente, Conta, Transacoes, Cliente, SolicitaCredito
 from PIL import ImageTk, Image
-from tinydb import TinyDB, where, Query
+from tinydb import TinyDB, where
 import tkinter.messagebox as messagebox
 import json
 
@@ -24,6 +25,40 @@ class AutocashApp:
         #         INÍCIO DAS FUNÇÕES         #
         ######################################
 
+        # ÍNCIO DO PAINEL GERENTE #
+        def abrir_painel_gerente(cliente_id):
+            imagem_tk = ImageTk.PhotoImage(imagem)
+            label = tk.Label(self.janela, image=imagem_tk)
+            label.place(x=0, y=0, relwidth=1, relheight=1)
+
+            def carregar_imagem():
+                imagem_original = Image.open(self.diretorio_atual + "/images/atm_bg.png")
+                nova_imagem = imagem_original.resize((600, 600))
+                nova_imagem = ImageTk.PhotoImage(nova_imagem)
+                label.config(image=nova_imagem)
+                label.image = nova_imagem    
+            carregar_imagem()
+
+            gerente = self.db.get(doc_id=cliente_id)
+            label_gerenre = tk.Label(self.janela, text='Olá, ' + gerente["nome"] + ".", font=('normal', 10), justify="left", bg="#50c7e2").place(x=120, y=50)
+
+            # Criando a barra de rolagem
+            scrollbar = ttk.Scrollbar(self.janela, orient=tk.VERTICAL)
+            scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+            # Criando a Listbox
+            listbox = tk.Listbox(self.janela, yscrollcommand=scrollbar.set)
+            listbox.pack(fill=tk.BOTH, expand=True)
+
+            # Adicionando itens à Listbox
+            for i in range(50):
+                listbox.insert(tk.END, f"Item {i+1}")
+            
+            listbox.place(x=70, y=80, width=300, height=150)
+
+            print('GERENTE ON!')
+
+
         # INÍCIO DA FUNÇÃO REALIZAR CADASTRO #
         def realizar_cadastro():
             subprocess.run(['python', self.diretorio_atual +'/cadastro.py'])
@@ -45,15 +80,21 @@ class AutocashApp:
             def logar():
                 cpf = entry_cpf_login.get()
                 senha = entry_senha.get()
-                cliente = self.db.search((where('cpf') == cpf) & (where('senha') == senha))
-                print(cliente)
-                if cliente:
-                    label_cliente['text'] = 'Login realizado com sucesso!'        
-                    label_cliente['bg'] = '#50c7e2'
-                    abrir_menu()  
-                else:
-                    label_cliente['text'] = 'CPF ou senha inválidos!'
-                    label_cliente['bg'] = '#50c7e2'
+                
+                for indice, cliente in enumerate(self.db.all()):
+                    if cliente['cpf'] == cpf and cliente['senha'] == senha:
+                        label_cliente['text'] = 'Login realizado com sucesso!'
+                        label_cliente['bg'] = '#50c7e2'
+                        cliente_id = indice+1
+                        usuario = self.db.get(doc_id=cliente_id)
+                        if usuario['cadastro_nivel'] == 2: 
+                            abrir_painel_gerente(cliente_id)
+                        else: 
+                            abrir_menu(cliente_id)                   
+                    else:
+                        label_cliente['text'] = 'CPF ou senha inválidos!'
+                        label_cliente['bg'] = '#50c7e2'
+
                         
             label_cpf_login= tk.Label(self.janela, text= 'CPF:', background="#50c7e2")
             label_cpf_login.pack()
@@ -80,9 +121,15 @@ class AutocashApp:
             button_9 = tk.Button(self.janela, text= '9', width=2).place(x=219, y=476)
             button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)
             button_enter = tk.Button(self.janela, text= 'Enter', command=logar).place(x=285, y=513)
+            button_asterisco = tk.Button(self.janela, text= '*', width=2).place(x=113, y=512)
+            button_hashtag = tk.Button(self.janela, text= '#').place(x=219, y=513)
 
             label_cliente = tk.Label(self.janela, text='', background="#50c7e2")
             label_cliente.place(x=100, y=260)
+
+
+
+
 
         # INÍCIO DA FUNÇÃO SAQUE #
         def abrir_saque():
@@ -92,12 +139,8 @@ class AutocashApp:
                 nova_imagem = ImageTk.PhotoImage(nova_imagem)
                 label.config(image=nova_imagem)
                 label.image = nova_imagem 
-            mudar_imagem()
 
-            imagem = Image.open(self.diretorio_atual + "/images/atm_bg.png")
-            imagem = imagem.resize((600, 600))
             imagem_tk = ImageTk.PhotoImage(imagem)
-
             label = tk.Label(self.janela, image=imagem_tk)
             label.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -143,73 +186,131 @@ class AutocashApp:
             button_enter = tk.Button(self.janela, text= 'Enter').place(x=285, y=513)
 
         # INÍCIO DA FUNÇÃO PAGAMENTO #
-        def exibir_mensagem(mensagem):
-                messagebox.showinfo("Mensagem", mensagem) 
-        def realizar_pagamento():
-            def mudar_imagem():
-                imagem_original = Image.open(self.diretorio_atual + "/images/atm_bg_pagamento.png")
-                nova_imagem = imagem_original.resize((600, 600))
-                nova_imagem = ImageTk.PhotoImage(nova_imagem)
-                label.config(image=nova_imagem)
-                label.image = nova_imagem
+        # def realizar_pagamento():
+        #     def mudar_imagem():
+        #         imagem_original = Image.open(self.diretorio_atual + "/images/atm_bg_pagamento.png")
+        #         nova_imagem = imagem_original.resize((600, 600))
+        #         nova_imagem = ImageTk.PhotoImage(nova_imagem)
+        #         label.config(image=nova_imagem)
+        #         label.image = nova_imagem
+        #     imagem = Image.open(self.diretorio_atual + "/images/atm_bg.png")
+        #     imagem = imagem.resize((600, 600))
+        #     imagem_tk = ImageTk.PhotoImage(imagem)
+        #     label = tk.Label(self.janela, image=imagem_tk)
+        #     label.place(x=0, y=0)
+        #     label.pack(fill="both", expand=True)
+
+        #     conta_origem_label = tk.Label(self.janela, text="Número da conta de origem:", background="#50c7e2")
+        #     conta_origem_label.pack()
+        #     conta_origem_label.place(x=100, y=90)
+
+        #     conta_origem_entry = tk.Entry(self.janela)
+        #     conta_origem_entry.pack()
+        #     conta_origem_entry.place(x=100, y=110)
+
+        #     conta_destino_label = tk.Label(self.janela, text="Número da conta de destino:", background="#50c7e2")
+        #     conta_destino_label.pack()
+        #     conta_destino_label.place(x=100, y=140)
+
+        #     conta_destino_entry = tk.Entry(self.janela)
+        #     conta_destino_entry.pack()
+        #     conta_destino_entry.place(x=100, y=160)
+
+        #     valor_label = tk.Label(self.janela, text="Valor do pagamento:", background="#50c7e2")
+        #     valor_label.pack()
+        #     valor_label.place(x=100, y=190)
+
+        #     valor_entry = tk.Entry(self.janela)
+        #     valor_entry.pack()
+        #     valor_entry.place(x=100, y=210)
+
+        #     agendar_var = tk.IntVar()
+        #     agendar_checkbox = tk.Checkbutton(self.janela, text="Agendar pagamento", variable=agendar_var, background="#50c7e2")
+        #     agendar_checkbox.place(x=100, y=240)
             
-            imagem = Image.open(self.diretorio_atual + "/images/atm_bg.png")
-            imagem = imagem.resize((600, 600))
-            imagem_tk = ImageTk.PhotoImage(imagem)
-            label = tk.Label(self.janela, image=imagem_tk)
-            label.place(x=0, y=0)
-            label.pack(fill="both", expand=True)
+        #     def processar_pagamento():
+        #         conta_origem = conta_origem_entry.get()
+        #         conta_destino = conta_destino_entry.get()
+        #         valor = float(valor_entry.get())
 
-            conta_origem_label = tk.Label(self.janela, text="Número da conta de origem:", background="#50c7e2")
-            conta_origem_label.place(x=100, y=90)
-
-            conta_origem_entry = tk.Entry(self.janela)
-            conta_origem_entry.place(x=100, y=110)
-
-            conta_destino_label = tk.Label(self.janela, text="Número da conta de destino:", background="#50c7e2")
-            conta_destino_label.place(x=100, y=140)
-
-            conta_destino_entry = tk.Entry(self.janela)
-            conta_destino_entry.place(x=100, y=160)
-
-            valor_label = tk.Label(self.janela, text="Valor do pagamento:", background="#50c7e2")
-            valor_label.place(x=100, y=190)
-
-            valor_entry = tk.Entry(self.janela)
-            valor_entry.place(x=100, y=210)
-
-            agendar_var = tk.IntVar()
-            agendar_checkbox = tk.Checkbutton(self.janela, text="Agendar pagamento", variable=agendar_var, background="#50c7e2")
-            agendar_checkbox.place(x=100, y=240)
+        #         if saldo_suficiente(conta_origem, valor):
+        #             atualizar_saldo(conta_origem, -valor)
+        #             atualizar_saldo(conta_destino, valor)
+        #             registrar_transacao(conta_origem, conta_destino, valor)
+        #             exibir_mensagem("Pagamento realizado com sucesso!")
+        #         else:
+        #             exibir_mensagem("Falha no pagamento: Saldo insuficiente.")
+        #     def atualizar_saldo(conta, valor):
+        #         with open('banco_de_dados.json', 'r') as file:
+        #             dados = json.load(file)
                 
-            button_1 = tk.Button(self.janela, text= '1', width=2).place(x=113, y=404)
-            button_2 = tk.Button(self.janela, text= '2', width=2).place(x=166, y=404)
-            button_3 = tk.Button(self.janela, text= '3', width=2).place(x=219, y=404)
-            button_4 = tk.Button(self.janela, text= '4', width=2).place(x=113, y=440)
-            button_5 = tk.Button(self.janela, text= '5', width=2).place(x=166, y=440)
-            button_6 = tk.Button(self.janela, text= '6', width=2).place(x=219, y=440)
-            button_7 = tk.Button(self.janela, text= '7', width=2).place(x=113, y=476)
-            button_8 = tk.Button(self.janela, text= '8', width=2).place(x=166, y=476)
-            button_9 = tk.Button(self.janela, text= '9', width=2).place(x=219, y=476)
-            button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)
-            button_enter = tk.Button(self.janela, text= 'Enter').place(x=285, y=513)
-            mudar_imagem()
-     
-        
+        #         saldo = dados['contas'][conta]['saldo']
+        #         saldo += valor
+        #         dados['contas'][conta]['saldo'] = saldo
+
+        #         with open('banco_de_dados.json', 'w') as file:
+        #             json.dump(dados, file)
+
+        #     def saldo_suficiente(conta, valor):
+        #         with open('banco_de_dados.json', 'r') as file:
+        #             dados = json.load(file)
+        #         if 'contas' in dados and conta in dados['contas']:
+        #             saldo = dados['contas'][conta]['saldo']
+        #             return saldo >= valor
+        #         else:
+        #             return False
+
+        #     def registrar_transacao(conta_origem, conta_destino, valor):
+        #         with open('banco_de_dados.json', 'r') as file:
+        #             dados = json.load(file)
+                
+        #         transacao = {
+        #             'conta_origem': conta_origem,
+        #             'conta_destino': conta_destino,
+        #             'valor': valor
+        #         }
+
+        #         dados['transacoes'].append(transacao)
+
+        #         with open('banco_de_dados.json', 'w') as file:
+        #             json.dump(dados, file)
+                    
+                    
+        #     button_1 = tk.Button(self.janela, text= '1', width=2).place(x=113, y=404)
+        #     button_2 = tk.Button(self.janela, text= '2', width=2).place(x=166, y=404)
+        #     button_3 = tk.Button(self.janela, text= '3', width=2).place(x=219, y=404)
+        #     button_4 = tk.Button(self.janela, text= '4', width=2).place(x=113, y=440)
+        #     button_5 = tk.Button(self.janela, text= '5', width=2).place(x=166, y=440)
+        #     button_6 = tk.Button(self.janela, text= '6', width=2).place(x=219, y=440)
+        #     button_7 = tk.Button(self.janela, text= '7', width=2).place(x=113, y=476)
+        #     button_8 = tk.Button(self.janela, text= '8', width=2).place(x=166, y=476)
+        #     button_9 = tk.Button(self.janela, text= '9', width=2).place(x=219, y=476)
+        #     button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)
+        #     button_enter = tk.Button(self.janela, text= 'Enter', command= processar_pagamento).place(x=285, y=513)
+        #     button_asterisco = tk.Button(self.janela, text= '*', width=2).place(x=113, y=512)
+        #     button_hashtag = tk.Button(self.janela, text= '#').place(x=219, y=513)
+
+            
+        #     def exibir_mensagem(mensagem):
+        #         messagebox.showinfo("Mensagem", mensagem) 
+                
         # INÍCIO DA FUNÇÃO MENU #
-        def abrir_menu():
+        def abrir_menu(cliente_id):
             imagem_tk = ImageTk.PhotoImage(imagem)
             label = tk.Label(self.janela, image=imagem_tk)
             label.place(x=0, y=0, relwidth=1, relheight=1)
+            cliente = self.db.get(doc_id=cliente_id)
+
+            label_cliente = tk.Label(self.janela, text='Olá, ' + cliente["nome"] + ".", font=('normal', 10), justify="left", bg="#50c7e2").place(x=120, y=50)
 
             opcoes_texto = "Menu:\n\n1 - Extrato\n2 - Saque\n3 - Depósito\n4 - Realizar pagamento\n5 - Solicitar crédito\n6 - Sair\n\nEntre com a sua opção: _ "
 
-            label_opcoes = tk.Label(self.janela, text=opcoes_texto, font=("Montserrat", 14), justify="left", bg="#50c7e2").place(x=70, y=90)
+            label_opcoes = tk.Label(self.janela, text=opcoes_texto, font=('normal', 13), justify="left", bg="#50c7e2").place(x=90, y=90)
 
             button_1 = tk.Button(self.janela, text= '1', width=2).place(x=113, y=404)
             button_2 = tk.Button(self.janela, text= '2', width=2, command=abrir_saque).place(x=166, y=404)
             button_3 = tk.Button(self.janela, text= '3', width=2).place(x=219, y=404)
-            button_4 = tk.Button(self.janela, text= '4', width=2, command=realizar_pagamento).place(x=113, y=440)
+            button_4 = tk.Button(self.janela, text= '4', width=2).place(x=113, y=440)
             button_5 = tk.Button(self.janela, text= '5', width=2).place(x=166, y=440)
             button_6 = tk.Button(self.janela, text= '6', width=2).place(x=219, y=440)
             button_7 = tk.Button(self.janela, text= '7', width=2).place(x=113, y=476)
@@ -217,25 +318,29 @@ class AutocashApp:
             button_9 = tk.Button(self.janela, text= '9', width=2).place(x=219, y=476)
             button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)
             button_enter = tk.Button(self.janela, text= 'Enter').place(x=285, y=513)
+            button_asterisco = tk.Button(self.janela, text= '*', width=2).place(x=113, y=512)
+            button_hashtag = tk.Button(self.janela, text= '#').place(x=219, y=513)
 
             self.janela.mainloop()
             
         while True:
-            opcoes_texto = 'Bem-vindo ao autocash.\nEscolha uma das opções abaixo:\n\n1 - Fazer login\n2 - Realizar cadastro'
+            opcoes_texto = 'Bem-vindo ao autocash.\nEscolha uma das opções abaixo:\n\n  1 - Fazer login\n  2 - Realizar cadastro'
 
-            label_opcoes = tk.Label(self.janela, text=opcoes_texto, font=("Montserrat", 14), justify="left", bg="#50c7e2", wraplength=300).place(x=80, y=90)
+            label_opcoes = tk.Label(self.janela, text=opcoes_texto, font=("normal", 14), justify="left", bg="#50c7e2", wraplength=300).place(x=80, y=90)
 
             button_1 = tk.Button(self.janela, text= '1', width=2, command=fazer_login).place(x=113, y=404)
             button_2 = tk.Button(self.janela, text= '2', width=2, command=realizar_cadastro).place(x=166, y=404)
-            button_3 = tk.Button(self.janela, text= '3', width=2, command=abrir_menu).place(x=219, y=404)
+            button_3 = tk.Button(self.janela, text= '3', width=2).place(x=219, y=404)
             button_4 = tk.Button(self.janela, text= '4', width=2).place(x=113, y=440)
             button_5 = tk.Button(self.janela, text= '5', width=2).place(x=166, y=440)
             button_6 = tk.Button(self.janela, text= '6', width=2).place(x=219, y=440)
             button_7 = tk.Button(self.janela, text= '7', width=2).place(x=113, y=476)
             button_8 = tk.Button(self.janela, text= '8', width=2).place(x=166, y=476)
             button_9 = tk.Button(self.janela, text= '9', width=2).place(x=219, y=476)
-            button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)            
+            button_0 = tk.Button(self.janela, text= '0', width=2).place(x=166, y=512)
             button_enter = tk.Button(self.janela, text= 'Enter').place(x=285, y=513)
+            button_asterisco = tk.Button(self.janela, text= '*', width=2).place(x=113, y=512)
+            button_hashtag = tk.Button(self.janela, text= '#').place(x=219, y=513)
 
             self.janela.mainloop()
 
